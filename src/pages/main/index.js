f61a7efd25b018a38874b643c6a89ed6b885bf67
Teckthor <teckthor@localhost.localdomain>
2019-08-24 01:29:11 -0300

    Adicioanando api Heremaps wheather

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,42 +1,47 @@
 import React, { Component } from 'react'
 import api from '../../services/api'
-import getLocation from '../../services/position'
+// import getLocation from '../../services/position'
 
 import './styles.css'
+// import { async } from 'q';
+// import { Promise } from 'q';
 
 export default class Main extends Component {
     state = {
-        temp: [],
+        city: '',
+        temperature: '',
+        observation: [],
     }
 
-    async componentDidMount() {
-        // this.loadTemp()
-        // const token = 'c7b70231067d16f99f4de4abc10ea7ae'
-        const token = 'f1c9c132c8a1e4458c2fb467e34db147'
-        const lat = -22.659225000000003
-        const long = -43.149291399999996
-
-        // const response = await api.get(`/weather?key=${token}`)
-        // const response = await api.get(`/weather?lat=${lat}&lon=${long}&appid=${token}`)
-        // const response = await api.get(`/api/v1/forecast/geo/hours/72?latitude=${lat}&longitude=${long}&token=${token}`)
-            
-        // this.setState({ temp: response.data })
+    componentDidMount() {
+        this.loadWheather()    
+    }
+    loadWheather = async () => {
         
-        // console.log(response.data)
-        api()
-        getLocation()
+        const resp = await api.get(`report.json?product=observation&name=Rio%20de%20janeiro&app_id=DswSqqnwDV2Ux2DtRtW1&app_code=dnmnNxuTzJt8Ki0fIk2pHw`)
+
+        this.setState({ 
+            city: resp.data.observations.location[0].city, 
+            temperature: resp.data.observations.location[0].observation[0].temperature,
+            observation: resp.data.observations.location[0].observation[0] 
+            
+        })
+        console.log(resp.data.observations.location[0].observation[0])
+        
     }
 
     render() {
+        const { temperature ,city, observation } = this.state
+        
         return (
-            
             <div className="main-content">
-                <p className="content" id="city">Magé</p>
-                <p className="content" id="date">Sex, 16 de agosto 00:22</p>
-                <p className="content" id="temp">15°c</p>
-                <p className="content" id="condition">
-                    {/* <i class="fas fa-cloud"></i>  */}
-                    Nublado</p>
+                <p className="content" id="city">{ city }</p>
+                {/* <p className="content" id="date">Sex, 16 de agosto 00:22</p> */}
+                <p className="content" id="temp">{ temperature.slice(0, 2) } °C</p>
+                <p className="content" id="condition"> { observation.description }</p>
+                <p>Temperatura minima: { observation.lowTemperature } °C</p>
+                <p>Temperatura maxima: { observation.highTemperature } °C</p>
+            {/* <img href={observation.iconLink} /> */}
             </div>
         )
     }
